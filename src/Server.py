116ae2a0deeaf18b610cd361264a3aeee52eaba5
superior_shrimp <szymon.zychowicz300701@gmail.com>
2023-06-05 16:53:56 +0200

    clear after execution

--- src/Server.py
+++ src/Server.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 import requests
 from uuid import uuid4
+import os
+import glob
+from time import sleep
 
 from VideoInfo import VideoInfo
 
@@ -8,21 +11,27 @@ from VideoInfo import VideoInfo
 
 app = FastAPI()
 
+VIDEOS_DIRECTORY = "../vids"
+
 SERVER = "http://localhost:8080/"
 
 VIDEOS = "videos/"
 OPTIMIZATIONS = "optimization/"
 
+async def delete_video(name): os.remove(name)
+
+async def clear_videos():
+    for f in glob.glob(VIDEOS_DIRECTORY): os.remove(f)
+
 @app.get("/")
-async def home():
-    return "Hello world!"
+async def home(): return "Hello world!"
 
 @app.post("/analysis")
 async def analysis_request(video_info: VideoInfo) -> str: # for now string; later proper object with car flow
     response = requests.get(SERVER + VIDEOS + video_info.id, stream=True)
     response.raise_for_status()
     
-    file_name = "../vids/vid" + video_info.id + "_" + str(uuid4()) + "." + video_info.extension
+    file_name = VIDEOS_DIRECTORY + "/vid" + video_info.id + "_" + str(uuid4()) + "." + video_info.extension
     
     print("Starting download: {} ({}B)".format(file_name, video_info.size))
     
@@ -36,4 +45,6 @@ async def analysis_request(video_info: VideoInfo) -> str: # for now string; late
     # model = Model(file_name)
     # model.run()
     
-    return "here"
+    await delete_video(file_name)
+    
+    return "in the future car flow here"
