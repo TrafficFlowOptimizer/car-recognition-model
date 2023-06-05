6ddee872e612d3014cdf2d2b83a3ee27b275d8f6
superior_shrimp <szymon.zychowicz300701@gmail.com>
2023-06-05 15:47:58 +0200

    dalej

--- src/Server.py
+++ src/Server.py
@@ -4,17 +4,22 @@ from uuid import uuid4
 
 from VideoInfo import VideoInfo
 
+# from Model import Model
+
 app = FastAPI()
 
-URL = "http://localhost:8080/videos/"
+SERVER = "http://localhost:8080/"
+
+VIDEOS = "videos/"
+OPTIMIZATIONS = "optimization/"
 
 @app.get("/")
 async def home():
     return "Hello world!"
 
-@app.post("/optimization")
-async def optimization_request(video_info: VideoInfo):
-    response = requests.get(URL + video_info.id, stream=True)
+@app.post("/analysis")
+async def analysis_request(video_info: VideoInfo) -> str: # for now string; later proper object with car flow
+    response = requests.get(SERVER + VIDEOS + video_info.id, stream=True)
     response.raise_for_status()
     
     file_name = "../vids/vid" + video_info.id + "_" + str(uuid4()) + "." + video_info.extension
@@ -22,9 +27,13 @@ async def optimization_request(video_info: VideoInfo):
     print("Starting download: {} ({}B)".format(file_name, video_info.size))
     
     with open(file_name, "wb") as video_file:
-        print(3)
         for chunk in response.iter_content(chunk_size=4096):
             if chunk:
                 video_file.write(chunk)
 
     print("Video {} successfully saved".format(video_info.id))
+    
+    # model = Model(file_name)
+    # model.run()
+    
+    return "here"
